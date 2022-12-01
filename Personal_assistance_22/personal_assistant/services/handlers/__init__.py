adbf19bf2047d741bf31110fefa1fc83fdb5a003
SSP0d <spodkopay@gmail.com>
2022-12-01 12:10:20 +0200

    fix

---
+++ Personal_assistance_22/personal_assistant/services/handlers/__init__.py
@@ -0,0 +1,27 @@
+from .common import (
+    hello,
+    help_command,
+    close_bot,
+    print_name,
+)
+from .contact import (
+    add_contact,
+    remove_contact,
+    show_all_users,
+    search_contacts,
+)
+from .phone import (
+    add_phone_to_contact,
+    change_contact_phone,
+    remove_contact_phone,
+    contact_phones,
+)
+from .birthday import (
+    change_contact_birthday,
+    days_before_birthday,
+)
+from .address import change_contact_address
+from .email import change_contact_email
+from .notes import (
+    add_note
+)
---
+++ run.py
@@ -0,0 +1,7 @@
+import time
+while True:
+    print('Helo, World!')
+    # var = input("Enter variables: ")
+    # if var == "exit":
+    time.sleep(20)
+        # break
